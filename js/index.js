e07f350bdb5fbd6dca54bc55c0385a5c4f4d826d
xo-shalaby-ox <mshalaby54@yahoo.com>
2024-11-25 07:14:59 +0200

    edit

--- js/index.js
+++ js/index.js
@@ -2,29 +2,33 @@ var webSiteName = document.getElementById("siteName");
 var webSiteUrl = document.getElementById("siteUrl");
 var popUp = document.getElementById("boxValid");
 var closeIcon = document.getElementById("close-popup");
+var listItems = document.getElementById("list-items");
+var CharIcon = document.querySelectorAll('.check-icon');
+var subBtn = document.getElementById("submitBtn");
+var updBtn = document.getElementById("updateBtn");
+var urlMsg = document.getElementById("urlMsg");
 
-var webSitesList = [];
 
-if(localStorage.getItem('sites') != null){
-  webSitesList = JSON.parse(localStorage.getItem('sites'));
+var webSitesList = JSON.parse(localStorage.getItem('sites')) || [];
+
+window.addEventListener("load", function () {
   display();
-}
-
+})
 // **************** Display Function ***************
 function createSiteLink() {
   var siteData = {
     siteName: webSiteName.value,
     siteURL: webSiteUrl.value
   }
-      if (nameValidate() == true && urlValidate() == true) {
-        webSitesList.push(siteData);
-        localStorage.setItem("sites", JSON.stringify(webSitesList));
-        webSiteName.classList.remove("is-valid");
-        webSiteUrl.classList.remove("is-valid");
-      }else {
-        popUp.classList.add("visible");
-        popUp.classList.remove("hidden");
-      }
+  if (nameValidate() == true && urlValidate() == true) {
+    webSitesList.push(siteData);
+    localStorage.setItem("sites", JSON.stringify(webSitesList));
+    webSiteName.classList.remove("is-valid");
+    webSiteUrl.classList.remove("is-valid");
+  } else {
+    popUp.classList.add("visible");
+    popUp.classList.remove("hidden");
+  }
   display();
   reset();
 }
@@ -33,34 +37,64 @@ function createSiteLink() {
 
 var nameRegExp = /^\w{3,}(\s+\w+)*$/;
 var urlRegExp = /^(https?:\/\/)?(w{3}\.)?\w+\.\w{2,}\/?(:\d{2,5})?(\/\w+)*$/;
+
 function nameValidate() {
+  for (var i = 0; i < CharIcon.length; i++) {
+    if (/[A-Z]/.test(siteName.value)) {
+      CharIcon[0].classList.add('checked');
+    } else {
+      CharIcon[0].classList.remove('checked');
+    }
+    if (/[a-z]/.test(siteName.value)) {
+      CharIcon[1].classList.add('checked');
+    } else {
+      CharIcon[1].classList.remove('checked');
+    }
+    if (/[0-9]/.test(siteName.value)) {
+      CharIcon[2].classList.add('checked');
+    } else {
+      CharIcon[2].classList.remove('checked');
+    }
+    if (/[_]/.test(siteName.value)) {
+      CharIcon[3].classList.add('checked');
+    } else {
+      CharIcon[3].classList.remove('checked');
+    }
+  }
+
   if (nameRegExp.test(webSiteName.value)) {
     webSiteName.classList.add("is-valid");
     webSiteName.classList.remove("is-invalid");
+    listItems.classList.add("hide-list");
+    listItems.classList.remove("show-list");
     return true;
   } else {
     webSiteName.classList.add("is-invalid");
     webSiteName.classList.remove("is-valid");
+    listItems.classList.add("show-list");
+    listItems.classList.remove("hide-list");
     return false;
   }
 }
 
 function urlValidate() {
-  if (urlRegExp.test(webSiteUrl.value) ) {
+  if (urlRegExp.test(webSiteUrl.value)) {
     webSiteUrl.classList.add("is-valid");
     webSiteUrl.classList.remove("is-invalid");
+    urlMsg.classList.add("d-none");
     return true;
   } else {
     webSiteUrl.classList.add("is-invalid");
     webSiteUrl.classList.remove("is-valid");
+    urlMsg.classList.remove("d-none");
     return false;
   }
 }
 // **************** Display Function ***************
-function display(){
-   var trs = ``;
-   for( var i = 0; i < webSitesList.length; i++){
-      trs += `
+function display() {
+  var trs = ``;
+  for (var i = 0; i < webSitesList.length; i++) {
+    trs += `
       <tr>
             <td>${i + 1}</td>
             <td>${webSitesList[i].siteName}</td>
@@ -76,11 +110,17 @@ function display(){
                   delete
                </button>
             </td>
+            <td>
+               <button onClick="updateOnSite(${i})" class="btn btn-outline-warning text-capitalize">
+                  <i class="fa-solid fa-pen-nib"></i>
+                  update
+               </button>
+            </td>
       </tr>
       `
-   }
+  }
 
-   document.getElementById("table-content").innerHTML = trs;
+  document.getElementById("table-content").innerHTML = trs;
 }
 
 //*************** clear Inputs Function ***************
@@ -99,15 +139,35 @@ function deleteSite(indexOfTable) {
 //*************** Visit Function ***************
 var linkRegExp = /^https?:\/\//;
 function visit(index) {
-    if (linkRegExp.test(webSitesList[index].siteURL)) {
-      open(`${webSitesList[index].siteURL}`);
-    } else {
-      open(`https://${webSitesList[index].siteURL}`);
-    }
+  if (linkRegExp.test(webSitesList[index].siteURL)) {
+    open(`${webSitesList[index].siteURL}`);
+  } else {
+    open(`https://${webSitesList[index].siteURL}`);
+  }
 }
 
 //*************** Close Function ***************
-function closePopUp(){
+function closePopUp() {
   popUp.classList.add("hidden");
   popUp.classList.remove("visible");
+}
+
+//*************** Update Function ***************
+var indexInfo;
+function updateOnSite(index) {
+  indexInfo = index;
+  webSiteName.value = webSitesList[index].siteName;
+  webSiteUrl.value = webSitesList[index].siteURL;
+
+  subBtn.style.display = "none";
+  updBtn.style.display = "block";
+}
+function update() {
+  webSitesList[indexInfo].siteName = webSiteName.value;
+  webSitesList[indexInfo].siteURL = webSiteUrl.value;
+  display(webSitesList);
+  localStorage.setItem("sites", JSON.stringify(webSitesList));
+  reset();
+  subBtn.style.display = "block";
+  updBtn.style.display = "none";
 }
